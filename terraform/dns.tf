fc02008e350cd1f13b70942a91de0ff778c8e0b9
mvaload <mvaload@gmail.com>
2022-12-12 22:31:13 +0300

    added DNS entry

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -7,12 +7,20 @@ resource "yandex_dns_zone" "zone1" {
 resource "yandex_dns_recordset" "rs1" {
   zone_id = yandex_dns_zone.zone1.id
   name    = "maopro.ru."
+  type    = "NS"
+  ttl     = 3600
+  data    = ["ns1.yandexcloud.net.", "ns2.yandexcloud.net."]
+}
+
+resource "yandex_dns_recordset" "rs2" {
+  zone_id = yandex_dns_zone.zone1.id
+  name    = "maopro.ru."
   type    = "A"
   ttl     = 200
   data    = [yandex_alb_load_balancer.project-balancer.listener[0].endpoint[0].address[0].external_ipv4_address[0].address]
 }
 
-resource "yandex_dns_recordset" "rs2" {
+resource "yandex_dns_recordset" "rs3" {
   zone_id = yandex_dns_zone.zone1.id
   name    = "maopro.ru."
   type    = "TXT"
